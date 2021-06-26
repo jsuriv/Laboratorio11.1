8fea886ccf61a2555e31a5dc9223b683bbf5fa8d
Steve <Steve@DESKTOP-JDB4ENH>
2021-06-26 12:25:33 -0400

    ..

--- PPacman/MapGenerator.cpp
+++ PPacman/MapGenerator.cpp
@@ -40,7 +40,7 @@ bool MapGenerator::load(string path)
 			{
 			case 'x':
 				objetoNuevo = new Pared(tileNuevo, textureManager->getTextura("pared_clasico"));
-				((GameActor*)objetoNuevo)->setFramesDireccion(4);
+				((GameActor*)objetoNuevo)->setFramesDireccion(8);
 				break;
 			case '.':
 				objetoNuevo = new Moneda(tileNuevo, textureManager->getTextura("moneda_clasico"));
